c338b38b451bbb7572b0500e74ea70de82986bbc
CesarRibeiro21 <cesartertulianort@gmail.com>
2024-10-21 11:54:07 -0300

    fazendo a pagina Sobre

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -1,8 +1,15 @@
-import './index.scss';
+import '../sobre/index.scss';
+import Cabecalho from '../../components/header/index.jsx'
+import Rodape from '../../components/footer/index.jsx';
 
 export default function Sobre(){
 
     return(
-        <div className='pagina-sobre'></div>
+         <div className='pagina-sobre'>
+           <Cabecalho/>
+            
+            
+
+        </div>
     )
 }
