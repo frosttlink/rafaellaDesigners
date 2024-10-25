4913cf7093acb13224385d31f254cef8be661959
Léo Alves <leolorena.alves@gmail.com>
2024-10-25 20:38:25 -0300

    estoque e melhorei a qualidade das img

--- src/pages/estoque/index.jsx
+++ src/pages/estoque/index.jsx
@@ -1,78 +1,110 @@
-
-import { ArrowLeftFromLine, Calendar, Layers, SquarePen, Undo2, UserRound, Image } from "lucide-react"
-import "./index.scss"
+import {
+  ArrowLeftFromLine,
+  Calendar,
+  Layers,
+  SquarePen,
+  Undo2,
+  UserRound,
+  Image,
+} from "lucide-react";
+import "./index.scss";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Estoque() {
-    const [menuOpcao, setmenuOpcao] = useState('');
-    const [nomeArquivo, setNomeArquivo] = useState("Nenhum arquivo selecionado")
+  const [menuOpcao, setmenuOpcao] = useState("");
+  const [nomeArquivo, setNomeArquivo] = useState("Nenhum arquivo selecionado");
+  const [menuCompacto, setMenuCompacto] = useState(false);
 
-    const handleFileChange = (e) => {
-        const file = e.target.files[0]
-        setNomeArquivo(file ? file.name : "Nenhuma arquivo selecionado")
-    }
-    return (
-        <div className="estoque-pagina">
-            <div className="secao">
-                <div className="menu">
-                    <header>
-                        <img src="/assets/images/borboleta2.svg" alt="" className="borboleta" /> <ArrowLeftFromLine className="min"/>
-                    </header>
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setNomeArquivo(file ? file.name : "Nenhuma arquivo selecionado");
+  };
+  return (
+    <div className="estoque-pagina">
+      <div className="secao">
+        <div className={`menu ${menuCompacto ? "compacto" : ""}`}>
+          <header onClick={() => setMenuCompacto(!menuCompacto)}>
+            <img
+              src="/assets/images/borboleta2.svg"
+              alt=""
+              className="borboleta"
+            />
+            <ArrowLeftFromLine className="min" />
+          </header>
 
-                    <ul>
-                        <li
-                            onClick={() => setmenuOpcao('agenda')}
-                            className={menuOpcao === 'agenda' ? 'active' : ''}>
-                            <Calendar className="icon" />  Agenda
-                        </li>
-                        <li
-                            onClick={() => setmenuOpcao('estoque')}
-                            className={menuOpcao === 'estoque' ? 'active' : ''}>
-                            <Layers className="icon" />  Estoque
-                        </li>
-                        <li
-                            onClick={() => setmenuOpcao('editar')}
-                            className={menuOpcao === 'editar' ? 'active' : ''}>
-                            <SquarePen className="icon" />  Editar site
-                        </li>
-                        <li
-                            onClick={() => setmenuOpcao('clientes')}
-                            className={menuOpcao === 'clientes' ? 'active' : ''}>
-                            <UserRound className="icon" />  Clientes
-                        </li>
-                    </ul>
+          <ul>
+            <li
+              onClick={() => setmenuOpcao("agenda")}
+              className={menuOpcao === "agenda" ? "active" : ""}
+            >
+              <Calendar className="icon" /> {!menuCompacto && "Agenda"}
+            </li>
+            <li
+              onClick={() => setmenuOpcao("estoque")}
+              className={menuOpcao === "estoque" ? "active" : ""}
+            >
+              <Layers className="icon" /> {!menuCompacto && "Estoque"}
+            </li>
+            <li
+              onClick={() => setmenuOpcao("editar")}
+              className={menuOpcao === "editar" ? "active" : ""}
+            >
+              <SquarePen className="icon" /> {!menuCompacto && "Editar site"}
+            </li>
+            <li
+              onClick={() => setmenuOpcao("clientes")}
+              className={menuOpcao === "clientes" ? "active" : ""}
+            >
+              <UserRound className="icon" /> {!menuCompacto && "Clientes"}
+            </li>
+          </ul>
 
-
-                    <button className="sair">Sair</button>
-
-                </div>
-
-                <div className="formularios">
-                    {menuOpcao == 'estoque' && (
-                        <form className="estoque-form">
-                            <Link>
-                                <Undo2 className="icon" />
-                            </Link>
-                            <input type="text" placeholder="Nome do produto" className="nome" />
-                            <input type="text" placeholder="Desrcrição do produto" className="descricao" />
-                            <div className="osDiferentes">
-                                <input type="number" placeholder="Categoria" className="categoria" />
-                                <input type="number" placeholder="Preço" className="preco" />
-                                <input type="number" placeholder="Quantidade" className="qtd" />
-                            </div>
-                            <div className="custom-file-input">
-                                <input type="file" id="fileInput" className="file-input" onChange={handleFileChange} />
-                                <label htmlFor="fileInput">
-                                    <span>{nomeArquivo}</span>
-                                    <Image className="icon" />
-                                </label>
-                            </div>
-                            <button className="reg">Registrar</button>
-                        </form>
-                    )}
-                </div>
-            </div>
+          <button className="sair">{!menuCompacto && "Sair"}</button>
         </div>
-    )
+
+        <div className="formularios">
+          {menuOpcao == "estoque" && (
+            <form className="estoque-form">
+              <Link>
+                <Undo2 className="icon" />
+              </Link>
+              <input
+                type="text"
+                placeholder="Nome do produto"
+                className="nome"
+              />
+              <input
+                type="text"
+                placeholder="Desrcrição do produto"
+                className="descricao"
+              />
+              <div className="osDiferentes">
+                <input
+                  type="text"
+                  placeholder="Categoria"
+                  className="categoria"
+                />
+                <input type="number" placeholder="Preço" className="preco" />
+                <input type="number" placeholder="Quantidade" className="qtd" />
+              </div>
+              <div className="custom-file-input">
+                <input
+                  type="file"
+                  id="fileInput"
+                  className="file-input"
+                  onChange={handleFileChange}
+                />
+                <label htmlFor="fileInput">
+                  <span>{nomeArquivo}</span>
+                  <Image className="icon" />
+                </label>
+              </div>
+              <button className="reg">Registrar</button>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -1,105 +1,141 @@
-  import { Link } from "react-router-dom";
-  import Cabecalho from "../../components/header";
-  import "./App.scss";
-  import { ChevronDown, CircleDollarSign, ShieldCheckIcon, ShoppingBasket } from "lucide-react";
-  import Rodape from "../../components/footer";
-  import ButtonVeja from "../../components/vejaButton";
-  import CardService from "../../components/cardService";
-  import DicasSection from "../../components/dicasSection";
-  import CardEncontra from "../../components/cardEncontra";
-  import AgendarButton from "../../components/agendarButton";
-  import Depoimentos from "../../components/depoimentsSection";
+import "./App.scss";
+import Cabecalho from "../../components/header";
+import {
+  ChevronDown,
+  CircleDollarSign,
+  ShieldCheckIcon,
+  ShoppingBasket,
+} from "lucide-react";
+import Rodape from "../../components/footer";
+import ButtonVeja from "../../components/vejaButton";
+import CardService from "../../components/cardService";
+import DicasSection from "../../components/dicasSection";
+import CardEncontra from "../../components/cardEncontra";
+import AgendarButton from "../../components/agendarButton";
+import Depoimentos from "../../components/depoimentsSection";
 
-  export default function Home() {
-    return (
-      <div className="pagina-home">
+import { motion } from "framer-motion";
+
+export default function Home() {
+  return (
+    <div className="pagina-home">
         <Cabecalho />
-        <div className="slogan">
-          <img src="/assets/images/busque.svg" alt="" className="busque" />
-          <p>
-            Transforme seu olhar com cílios e sobrancelhas perfeitos
-            <br />
-            Realçamos sua beleza natural, destacando o que há de mais encantador
-            em você.
-          </p>
-          <ButtonVeja tamBotao="140px" texto="Veja mais" />
-          <center>
-            <a href="#dicas">
-              <ChevronDown className="roll" />
-            </a>
-          </center>
-        </div>
-
-        <DicasSection />
-
-        <img src="/assets/images/line_one.svg" alt="" className="line_one" />
-
-        <div className="slogan-service">
-          <img src="/assets/images/services.svg" alt="" />
-          <p>
-            Oferecemos cuidados especializados para realçar sua beleza, desde
-            alongamento de cílios, design de sobrancelhas até epilação. Usamos
-            produtos de alta qualidade para garantir resultados duradouros e
-            naturais.
-          </p>
-        </div>
-        <div className="cards">
-          <CardService
-            caminho="/assets/images/image.png"
-            preco="R$1"
-            caminhoServico="/assets/images/cilios.svg"
-            texto="Serviço de estética de cílios envolve alongamento, lifting ou tintura para realçar os cílios, tornando-os mais longos, volumosos e definidos, com resultados naturais e duradouros."
-          />
-          <CardService
-            caminho="/assets/images/image2.png"
-            preco="R$1"
-            caminhoServico="/assets/images/sobrancelha.svg"
-            texto="Serviço de estética de sobrancelhas inclui design, micropigmentação ou tintura para definir e realçar o formato das sobrancelhas, garantindo uma aparência harmoniosa e bem cuidada."
-          />
-          <CardService
-            caminho="/assets/images/imag3.png"
-            preco="R$1"
-            caminhoServico="/assets/images/epilacao.svg"
-            texto="A epilação é o processo de remoção dos pelos pela raiz, utilizando métodos como cera quente, pinça, ou aparelhos elétricos. Ao contrário da depilação, que apenas corta o pelo na superfície."
-          />
-        </div>
+        <AgendarButton />
+      <div className="slogan">
+        <img src="/assets/images/busque.svg" alt="" className="busque" />
+        <p>
+          Transforme seu olhar com cílios e sobrancelhas perfeitos
+          <br />
+          Realçamos sua beleza natural, destacando o que há de mais encantador
+          em você.
+        </p>
+        <ButtonVeja tamBotao="140px" texto="Veja mais" />
         <center>
-          {" "}
-          <ButtonVeja texto="Veja os detalhes" />{" "}
+          <a href="#dicas">
+            <ChevronDown className="roll" />
+          </a>
+        </center>
+      </div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+      >
+        <DicasSection />
+      </motion.div>
+
+      <img src="/assets/images/line_one.svg" alt="" className="line_one" />
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        className="slogan-service"
+      >
+        <img src="/assets/images/services.svg" alt="" />
+        <p>
+          Oferecemos cuidados especializados para realçar sua beleza, desde
+          alongamento de cílios, design de sobrancelhas até epilação. Usamos
+          produtos de alta qualidade para garantir resultados duradouros e
+          naturais.
+        </p>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        className="cards"
+      >
+        <CardService
+          caminho="/assets/images/image.svg"
+          preco="R$1"
+          caminhoServico="/assets/images/cilios.svg"
+          texto="Serviço de estética de cílios envolve alongamento, lifting ou tintura para realçar os cílios, tornando-os mais longos, volumosos e definidos, com resultados naturais e duradouros."
+        />
+        <CardService
+          caminho="/assets/images/image2.svg"
+          preco="R$1"
+          caminhoServico="/assets/images/sobrancelha.svg"
+          texto="Serviço de estética de sobrancelhas inclui design, micropigmentação ou tintura para definir e realçar o formato das sobrancelhas, garantindo uma aparência harmoniosa e bem cuidada."
+        />
+        <CardService
+          caminho="/assets/images/image3.svg"
+          preco="R$1"
+          caminhoServico="/assets/images/epilacao.svg"
+          texto="A epilação é o processo de remoção dos pelos pela raiz, utilizando métodos como cera quente, pinça, ou aparelhos elétricos. Ao contrário da depilação, que apenas corta o pelo na superfície."
+        />
+      </motion.div>
+      <center>
+        <ButtonVeja texto="Veja os detalhes" />
+      </center>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+      >
+        <Depoimentos />
+      </motion.div>
+      
+      <img src="/assets/images/line_two.svg" alt="" className="line_two" />
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        className="vantagens"
+      >
+        <center>
+          <img src="/assets/images/encontra.svg" alt="" className="encontra" />
         </center>
 
-        <div>
-          <Depoimentos />
+        <div className="cards_encontra">
+          <CardEncontra
+            icone={ShieldCheckIcon}
+            imagem="certi.svg"
+            texto="Tenho certificação em design de sobrancelhas e extensão de cílios, garantindo um atendimento profissional e qualificado."
+          />
+          <CardEncontra
+            icone={CircleDollarSign}
+            imagem="preco.svg"
+            texto="Ofereço serviços de sobrancelhas e cílios com preços acessíveis, sem comprometer a qualidade e o cuidado."
+          />
+          <CardEncontra
+            icone={ShoppingBasket}
+            imagem="produto.svg"
+            texto="Uso produtos de qualidade para garantir os melhores resultados em sobrancelhas e cílios, sempre mantendo preços acessíveis."
+          />
         </div>
-        <img src="/assets/images/line_two.svg" alt="" className="line_two" />
+      </motion.div>
 
-        <div className="vantagens">
-          <center>
-            <img src="/assets/images/encontra.svg" alt="" className="encontra" />
-          </center>
-
-          <div className="cards_encontra">
-            <CardEncontra 
-              icone={ShieldCheckIcon}
-              imagem="certi.svg" 
-              texto="Tenho certificação em design de sobrancelhas e extensão de cílios, garantindo um atendimento profissional e qualificado." 
-            />
-            <CardEncontra 
-              icone={CircleDollarSign}
-              imagem="preco.svg" 
-              texto="Ofereço serviços de sobrancelhas e cílios com preços acessíveis, sem comprometer a qualidade e o cuidado." 
-            />
-            <CardEncontra 
-              icone={ShoppingBasket}
-              imagem="produto.svg" 
-              texto="Uso produtos de qualidade para garantir os melhores resultados em sobrancelhas e cílios, sempre mantendo preços acessíveis."
-            />
-
-            <AgendarButton />
-          </div>
-        </div>
-
-        <Rodape />
-      </div>
-    );
-  }
+      <Rodape />
+    </div>
+  );
+}
